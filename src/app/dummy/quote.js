621fcce69bf0abed2fcc246baaff7bdf993f9509
AbuDzarAlGhifari <rasduba@gmail.com>
2024-09-21 23:30:39 +0700

    update: button

---
+++ src/app/dummy/quote.js
@@ -0,0 +1,166 @@
+const quote = [
+  {
+    id: '1',
+    quote:
+      "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle.",
+    nama: 'Steve Jobs',
+  },
+  {
+    id: '2',
+    quote:
+      'All our dreams can come true, if we have the courage to pursue them.',
+    nama: 'Walt Disney',
+  },
+  {
+    id: '3',
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    nama: 'Thomas Edison',
+  },
+  {
+    id: '4',
+    quote:
+      'Choose a job you love, and you will never have to work a day in your life.',
+    nama: 'Confucius',
+  },
+  {
+    id: '5',
+    quote: 'Nothing will work unless you do.',
+    nama: 'Maya Angelou',
+  },
+  {
+    id: '6',
+    quote: 'In the middle of difficulty lies opportunity.',
+    nama: 'Albert Einstein',
+  },
+  {
+    id: '7',
+    quote:
+      'The biggest adventure you can take is to live the life of your dreams.',
+    nama: 'Oprah Winfrey',
+  },
+  {
+    id: '8',
+    quote:
+      'Success is not final, failure is not fatal: it is the courage to continue that counts.',
+    nama: 'Winston Churchill',
+  },
+  {
+    id: '9',
+    quote: 'The future depends on what you do today.',
+    nama: 'Mahatma Gandhi',
+  },
+  {
+    id: '10',
+    quote: "Believe you can and you're halfway there.",
+    nama: 'Theodore Roosevelt',
+  },
+  {
+    id: '11',
+    quote:
+      'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.',
+    nama: 'Steve Jobs',
+  },
+  {
+    id: '12',
+    quote:
+      'The only place where success comes before work is in the dictionary.',
+    nama: 'Vidal Sassoon',
+  },
+  {
+    id: '13',
+    quote: "Don't watch the clock; do what it does. Keep going.",
+    nama: 'Sam Levenson',
+  },
+  {
+    id: '14',
+    quote: 'The best way to predict the future is to create it.',
+    nama: 'Peter Drucker',
+  },
+  {
+    id: '15',
+    quote: 'The harder I work, the luckier I get.',
+    nama: 'Gary Player',
+  },
+  {
+    id: '16',
+    quote: "Opportunities don't happen. You create them.",
+    nama: 'Chris Grosser',
+  },
+  {
+    id: '17',
+    quote:
+      "Success isn't about how much money you make; it's about the difference you make in people's lives.",
+    nama: 'Michelle Obama',
+  },
+  {
+    id: '18',
+    quote:
+      'The only limit to our realization of tomorrow will be our doubts of today.',
+    nama: 'Franklin D. Roosevelt',
+  },
+  {
+    id: '19',
+    quote:
+      'Do not wait to strike till the iron is hot; but make it hot by striking.',
+    nama: 'William Butler Yeats',
+  },
+  {
+    id: '20',
+    quote: "Your time is limited, don't waste it living someone else's life.",
+    nama: 'Steve Jobs',
+  },
+  {
+    id: '21',
+    quote: 'The secret of getting ahead is getting started.',
+    nama: 'Mark Twain',
+  },
+  {
+    id: '22',
+    quote:
+      'The only person you are destined to become is the person you decide to be.',
+    nama: 'Ralph Waldo Emerson',
+  },
+  {
+    id: '23',
+    quote: "Don't be afraid to give up the good to go for the great.",
+    nama: 'John D. Rockefeller',
+  },
+  {
+    id: '24',
+    quote: 'I find that the harder I work, the more luck I seem to have.',
+    nama: 'Thomas Jefferson',
+  },
+  {
+    id: '25',
+    quote:
+      'The difference between ordinary and extraordinary is that little extra.',
+    nama: 'Jimmy Johnson',
+  },
+  {
+    id: '26',
+    quote: "If opportunity doesn't knock, build a door.",
+    nama: 'Milton Berle',
+  },
+  {
+    id: '27',
+    quote: 'The way to get started is to quit talking and begin doing.',
+    nama: 'Walt Disney',
+  },
+  {
+    id: '28',
+    quote: "Don't let yesterday take up too much of today.",
+    nama: 'Will Rogers',
+  },
+  {
+    id: '29',
+    quote: "You miss 100% of the shots you don't take.",
+    nama: 'Wayne Gretzky',
+  },
+  {
+    id: '30',
+    quote: 'The best preparation for tomorrow is doing your best today.',
+    nama: 'H. Jackson Brown Jr.',
+  },
+];
+
+export default quote;
